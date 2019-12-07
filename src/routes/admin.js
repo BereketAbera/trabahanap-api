43ee3581270e9917d60cbcc5d29ce9129e6aadc9
Silas <silogecho@gmail.com>
2019-12-07 11:03:35 +0300

    Create Location form Admin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,6 +1,8 @@
 const app = module.exports = require('express')();
 const otherController = require('../controllers/other.controller');
 const userController = require('../controllers/users.controller');
+const locationController = require('../controllers/locations.controller')
+const jobsController = require('../controllers/jobs.controller')
 
 app.get('/employers', otherController.getEmployers);
 app.post('/employer',userController.admnCreateCompanyProfileWithBusinessLicenseAndLogo);
@@ -11,3 +13,9 @@ app.post('/issue_responses', otherController.addIssueResponse);
 
 app.post('/applicants', userController.createApplicant)
 app.get('/applicants', userController.getApplicants)
+
+app.post('/location', locationController.addLocationWithImage);
+app.post('/jobs', jobsController.addJob);
+
+
+
