d6ea42c94f4fa2aa8169d0a1f0b415a58fc6a12c
Silas <silogecho@gmail.com>
2020-01-13 17:45:55 +0300

    database swtich

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("trabahanap_migration", "root", 'password', {host: '192.168.0.10', dialect: 'mysql', logging: false});
+// const sequelize = new Sequelize("trabahanap_migration", "root", 'password', {host: '192.168.0.10', dialect: 'mysql', logging: false});
 
 const sequelize = new Sequelize("trabahanap_development", "root", "", {host: "localhost", dialect: "mysql", logging: false,operatorsAliases:1});
 //const sequelize = new Sequelize("trabahanap_migration", "root", "password", {host: "192.168.0.10", dialect: "mysql", logging: false,operatorsAliases:1});
