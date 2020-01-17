95b2c8847bf9fa81de9bc49d541431b3baac248d
Silas <silogecho@gmail.com>
2020-01-17 17:06:30 +0300

    add featured api

--- src/routes/anonymous.js
+++ src/routes/anonymous.js
@@ -7,7 +7,8 @@ const otherController = require('../controllers/other.controller');
 app.get('/jobs/:id', jobsController.getJob);
 app.get('/jobs', jobsController.getAllJobs);
 app.get('/employers/featured', otherController.getFeaturedCompanies);
-app.get('/jobs/:featureId',jobsController.adminGetAllCompanyJob)
+app.get('/jobs/:featureId',jobsController.adminGetAllCompanyJob);
+app.get('/company/:companyProfileId',otherController.getCompanyDetails);
 
 app.get('/location/cities', locationsController.getAllCities);
 app.get('/location/regions', locationsController.getAllRegions);
