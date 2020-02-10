66a278f46ee60da508a83100b6f4362ad376abf7
Silas <silogecho@gmail.com>
2020-02-10 11:21:16 +0300

    add constant

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@
 
 const baseDir = __dirname;
 
-const HOST_URL = "https://trabahanap.com//api";
+const HOST_URL = "https://trabahanap.com/api";
 const HOST_URL_FRONTEND = "https://trabahanap.com";
 const JWTSECRET = "ad4f783b-bbc9-43a1-88a4-1bec8da0f363";
 const JWTPASSWORDSECRET = "e379a9d1-3a9b-4853-8559-fafbf31d7b43";
